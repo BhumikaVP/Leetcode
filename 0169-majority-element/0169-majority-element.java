bfa5ee214325dfbab548a18c51c2fc3b080718ad
BhumikaVP <bhumikavp04@gmail.com>
2026-07-28 11:37:31 -0700

    Time: 12 ms (37.31%), Space: 52.9 MB (73.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,14 +3,16 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
         }
-        for(Integer key:map.keySet()){
-            if(map.get(key)>nums.length/2){
-                return key;
+        for(Integer keys: map.keySet()){
+            if(map.get(keys)>nums.length/2){
+                return keys;
             }
         }
         return -1;
         
         
     }
+
 }
